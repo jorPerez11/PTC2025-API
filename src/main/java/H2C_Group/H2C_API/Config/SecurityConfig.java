8f4235cdc719130d11f485a04c2b4f559fc6a7f6
Astrid <20240235.ricaldone.edu.sv>
2025-10-11 11:57:28 -0600

    Cambios Web 11/10

--- src/main/java/H2C_Group/H2C_API/Config/SecurityConfig.java
+++ src/main/java/H2C_Group/H2C_API/Config/SecurityConfig.java
@@ -147,7 +147,7 @@ public class SecurityConfig{
                 "http://127.0.0.2:5501",
 
 
-                // Orígenes sin puerto (si accedes a la página directamente por localhost)
+                // Orígenes sin puerto (vercel y localhost)
                 "http://localhost",
                 "https://localhost",
                 "https://*.herokuapp.com",
